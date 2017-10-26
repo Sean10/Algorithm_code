9da5ba553587f445227afabb2d5c4922e203de2f
sean10 <sean10reborn@gmail.com>
2017-10-26 22:53:45 +0800

    optimizing leetcode 2

--- cpp/LeetCode_2.cpp
+++ cpp/LeetCode_2.cpp
@@ -8,6 +8,8 @@ leetCode 2
 
 2. 除了大数，还会有什么边界情况呢？似乎是没有了，题目中也写到了是非负.
 
+看了Solution……我的思路太没结构了……居然那么简单
+
 
  */
 
@@ -36,108 +38,127 @@ struct ListNode {
  class Solution {
  public:
      ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+         ListNode* ans = new ListNode(0);
          int carry = 0;
-
-
-         ListNode* ans_list = new ListNode(0);
-         ListNode* curr_ans = ans_list;
-
-         int head_sum = l1->val + l2->val;
-
-
-         if(head_sum >= 10)
+         ListNode* p = l1;
+         ListNode* q = l2;
+         ListNode* curr = ans;
+         while(p != NULL || q != NULL)
          {
-             carry = 1;
+             int x = (p != NULL) ? p->val : 0;
+             int y = (q != NULL) ? q->val : 0;
+             int sum = carry + x+ y;
+             carry = sum/10;
+             curr->next = new ListNode(sum%10);
+             curr = curr->next;
+             if(p != NULL) p = p->next;
+             if(q != NULL) q = q->next;
          }
-
-         ans_list->val = head_sum%10;
-         ans_list->next = NULL;
-
-         //cout << __LINE__ << " "<< ans_list->val << endl;
-
-
-         ListNode* curr_l1 = l1->next;
-         ListNode* curr_l2 = l2->next;
-
-
-         int tmp_sum = 0;
-         while(curr_l1 != NULL && curr_l2 != NULL)
+         if(carry > 0)
          {
-             ListNode* p = new ListNode(0);
-
-             tmp_sum = curr_l1->val + curr_l2->val + carry;
-
-             if(tmp_sum >= 10)
-             {
-                 carry = 1;
-             }else
-             {
-                 carry = 0;
-             }
-
-             p->val = tmp_sum%10;
-             p->next = NULL;
-             curr_ans->next = p;
-             curr_ans = p;
-
-             curr_l1 = curr_l1->next;
-             curr_l2 = curr_l2->next;
+             curr->next = new ListNode(carry);
          }
-
-         if(curr_l1 == NULL && curr_l2 == NULL && carry == 1)
-         {
-             ListNode* p = new ListNode(0);
-             p->val = 1;
-             p->next = NULL;
-             curr_ans->next = p;
-         }
-         else if(curr_l2 == NULL || curr_l1 == NULL)
-         {
-             if(curr_l2 == NULL)
-             {
-                 curr_ans->next = curr_l1;
-             }
-             else if(curr_l1 == NULL)
-             {
-                 curr_ans->next = curr_l2;
-             }
-
-             while(carry)
-             {
-                 int tmp_sum;
-
-                 if(curr_ans->next == NULL)
-                 {
-                     tmp_sum = carry;
-                     ListNode* p = new ListNode(0);
-                     p->val = tmp_sum;
-                     p->next = NULL;
-                     curr_ans->next = p;
-                     curr_ans = curr_ans->next;
-                 }
-                 else
-                 {
-                     curr_ans = curr_ans->next;
-                     tmp_sum = curr_ans->val + carry;
-                     curr_ans->val = tmp_sum%10;
-                 }
-
-                 //cout << __LINE__ << " "<< tmp_sum << endl;
-
-
-                 if(tmp_sum >= 10)
-                 {
-                     carry = 1;
-                 }else{
-                     carry = 0;
-                 }
-                 //curr_ans = curr_ans->next;
-             }
-         }
-
-         return ans_list;
-
+         return ans->next;
      }
+
+
+        //  int carry = 0;
+         //
+        //  ListNode* ans_list = new ListNode(0);
+        //  ListNode* curr_ans = ans_list;
+         //
+        //  int head_sum = l1->val + l2->val;
+         //
+         //
+        //  if(head_sum >= 10)
+        //  {
+        //      carry = 1;
+        //  }
+         //
+        //  ans_list->val = head_sum%10;
+        //  ans_list->next = NULL;
+         //
+        //  ListNode* curr_l1 = l1->next;
+        //  ListNode* curr_l2 = l2->next;
+         //
+         //
+        //  int tmp_sum = 0;
+        //  while(curr_l1 != NULL && curr_l2 != NULL)
+        //  {
+        //      ListNode* p = new ListNode(0);
+         //
+        //      tmp_sum = curr_l1->val + curr_l2->val + carry;
+         //
+        //      if(tmp_sum >= 10)
+        //      {
+        //          carry = 1;
+        //      }else
+        //      {
+        //          carry = 0;
+        //      }
+         //
+        //      p->val = tmp_sum%10;
+        //      p->next = NULL;
+        //      curr_ans->next = p;
+        //      curr_ans = p;
+         //
+        //      curr_l1 = curr_l1->next;
+        //      curr_l2 = curr_l2->next;
+        //  }
+         //
+        //  if(curr_l1 == NULL && curr_l2 == NULL && carry == 1)
+        //  {
+        //      ListNode* p = new ListNode(0);
+        //      p->val = 1;
+        //      p->next = NULL;
+        //      curr_ans->next = p;
+        //  }
+        //  else if(curr_l2 == NULL || curr_l1 == NULL)
+        //  {
+        //      if(curr_l2 == NULL)
+        //      {
+        //          curr_ans->next = curr_l1;
+        //      }
+        //      else if(curr_l1 == NULL)
+        //      {
+        //          curr_ans->next = curr_l2;
+        //      }
+         //
+        //      while(carry)
+        //      {
+        //          int tmp_sum;
+         //
+        //          if(curr_ans->next == NULL)
+        //          {
+        //              tmp_sum = carry;
+        //              ListNode* p = new ListNode(0);
+        //              p->val = tmp_sum;
+        //              p->next = NULL;
+        //              curr_ans->next = p;
+        //              curr_ans = curr_ans->next;
+        //          }
+        //          else
+        //          {
+        //              curr_ans = curr_ans->next;
+        //              tmp_sum = curr_ans->val + carry;
+        //              curr_ans->val = tmp_sum%10;
+        //          }
+         //
+        //          //cout << __LINE__ << " "<< tmp_sum << endl;
+         //
+         //
+        //          if(tmp_sum >= 10)
+        //          {
+        //              carry = 1;
+        //          }else{
+        //              carry = 0;
+        //          }
+        //          //curr_ans = curr_ans->next;
+        //      }
+        //  }
+         //
+        //  return ans_list;
+        //  }
 
  };
 
