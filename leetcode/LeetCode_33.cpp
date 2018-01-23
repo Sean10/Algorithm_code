4f041b4642b8a7dbe7e5f0afd47ecdd3dba7ba0a
sean10 <sean10reborn@gmail.com>
2018-01-23 22:16:11 +0800

    exercise

--- leetcode/LeetCode_33.cpp
+++ leetcode/LeetCode_33.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size()-1;
+        int low = 0, high = nums.size();
         while(low < high)
         {
             int mid = (low+high)/2;
