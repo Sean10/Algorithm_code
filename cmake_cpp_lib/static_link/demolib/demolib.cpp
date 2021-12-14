2a0facf00ef21bb421e2240bb48efb5c026baf22
sean10 <sean10reborn@gmail.com>
2021-12-14 15:31:07 +0800

    add static lib dynamic link to so

---
+++ cmake_cpp_lib/static_link/demolib/demolib.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../demolib2/demolib.h"
+using namespace std;
+
+void print_hello() {
+    cout << "hello word" << endl;
+    print_hello2();
+}
---
+++ cmake_cpp_lib/static_link/demolib2/demolib.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+void print_hello2() {
+    cout << "hello word demolib2" << endl;
+}
